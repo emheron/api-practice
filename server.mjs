a1c47d66b7dafbbccbaa9a96597e952122eaa46e
emheron <m.emmanuel.heron@gmail.com>
2023-08-19 17:34:17 -0500

    serve static file from current dir

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ let weatherApiKey = process.env.WEATHER_API_KEY || '';
 let apodApiKey = process.env.APOD_API_KEY || '';
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 app.get('/weather', async (req, res) => {
     const city = req.query.city;
