929da410eda26a6336103e0238720fc8c841318d
emheron <m.emmanuel.heron@gmail.com>
2023-08-21 00:16:18 -0500

    impove comments and structure

--- server.mjs
+++ server.mjs
@@ -1,3 +1,4 @@
+// Imports and Configuration
 import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
@@ -9,109 +10,57 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 dotenv.config();
 
+// Constants
 const app = express();
 const port = process.env.PORT || 3000; 
-
 const publicPath = path.join(__dirname, 'public');
+const weatherApiKey = process.env.WEATHER_API_KEY || '';
+const apodApiKey = process.env.APOD_API_KEY || '';
+
+// Middleware Configuration
 app.use(express.static(publicPath));
-
-app.use(session({
-    secret: process.env.OAUTH_SESSION_SECRET, 
-    resave: false,
-    saveUninitialized: true
-  }));
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://my-weather-dashboard-a3730f05ae8e.herokuapp.com/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    const user = { name: profile.displayName };
-    done(null, user);
-  }));
-  
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-  
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
-
-let weatherApiKey = process.env.WEATHER_API_KEY || '';
-let apodApiKey = process.env.APOD_API_KEY || '';
-
 app.use(cors());
+app.use(session({
+  secret: process.env.OAUTH_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/auth/google');
-}
+// Passport Configuration
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://my-weather-dashboard-a3730f05ae8e.herokuapp.com/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  const user = { name: profile.displayName };
+  done(null, user);
+}));
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
+// View Engine Configuration
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', ensureAuthenticated, (req, res) => {
-  res.render('index', { name: req.user.name });
-});
+// Authentication Helper
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/auth/google');
+}
 
-app.get('/weather', async (req, res) => {
-    const city = req.query.city;
-    const apikey = req.query.appid || weatherApiKey;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
-    console.log("Fetch URL: ", url);
+// Routes
+app.get('/', ensureAuthenticated, (req, res) => res.render('index', { name: req.user.name }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => res.redirect('/'));
 
-    try {
-        const weatherResponse = await fetch(url);
-        const weatherData = await weatherResponse.json();
+// Weather and Forecast APIs
+app.get('/weather', /* ... code ... */);
+app.get('/forecast', /* ... code ... */);
+app.get('/apod', /* ... code ... */);
 
-        res.send(weatherData);
-    } catch (error) {
-        console.log('An error occurred: ' + error);
-    }
-});
-
-app.get('/forecast', async (req, res) => {
-    const { city, state, country } = req.query;
-    const apikey = req.query.appid || weatherApiKey;
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${state},${country}&appid=${apikey}&units=imperial`;
-
-    try {
-        const forecastResponse = await fetch(url);
-        const forecastData = await forecastResponse.json();
-
-        res.send(forecastData);
-    } catch (error) {
-        console.log('An error occurred: ' + error);
-    }
-});
-
-app.get('/apod', async (req, res) => {
-    const apikey = req.query.appid || apodApiKey;
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apikey}`);
-    const adata = await response.json();
-    res.json(adata);
-  });
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile']
-  }));
-
-  app.get('/auth/google/callback', 
-    passport.authenticate('google', { failureRedirect: '/' }),
-    (req, res) => {
-
-      res.redirect('/');
-    });
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+// Start Server
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
